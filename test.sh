a7877424a6949867994fc95fb9dc1f09110453a9
Alexey Busygin <yaabusygin@gmail.com>
2023-01-25 08:39:03 +0300

    Use POSIX compatible array of strings

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-platforms="geerlingguy/docker-debian11-ansible:latest \
-           geerlingguy/docker-ubuntu2204-ansible:latest"
+# platform images
+set -- \
+    geerlingguy/docker-debian11-ansible:latest \
+    geerlingguy/docker-ubuntu2204-ansible:latest
 
-for image in ${platforms}; do
+for image in "${@}"; do
     echo "Running Molecule tests for '${image}'..."
     TEST_IMAGE="${image}" molecule test --all
     if [ $? -ne 0 ]; then
